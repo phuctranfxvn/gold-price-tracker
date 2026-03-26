2fa70ddcd3edf8e7fd0837793f8ab4ea740a6e55
Phuc Tran Thanh <phuctran.fx.vn@gmail.com>
2026-03-26 18:43:22 +0700

    [ADD] Add mihong branch

--- scheduler_runner.py
+++ scheduler_runner.py
@@ -1,9 +1,9 @@
-from app import job_fetch_and_store, init_db, fetch_price_for_date, insert_price
+from app import job_fetch_and_store, init_db
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 if __name__ == "__main__":
-    print(">>> Starting background scheduler (every 30 mins)")
     init_db()
+    print(">>> Starting scheduler (every 30 minutes)")
     scheduler = BlockingScheduler()
-    scheduler.add_job(job_fetch_and_store, 'interval', hours=0.5)
+    scheduler.add_job(job_fetch_and_store, 'cron', minute='0,30', timezone='Asia/Ho_Chi_Minh')
     scheduler.start()
